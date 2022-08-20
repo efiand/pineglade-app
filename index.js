b8e77c87fef30460b7bae5b4ff5da79499aa22fb
Andrey Efimov <efiand@ya.ru>
2022-08-20 14:24:50 +0300

    new app

--- index.js
+++ index.js
@@ -1,30 +1,19 @@
-import { isCompile, isDev, isSelf } from './constants.js';
-import defineTasks from './lib/defineTasks.js';
-import getFullPath from './lib/getFullPath.js';
-import log from './lib/log.js';
+import { isCompile, isSelf, isTest } from './constants.js';
+import app from './lib/app.js';
 
 (async () => {
+	await app.configure();
+
 	if (isSelf) {
-		return await defineTasks().lintSelf();
+		return await app.lintSelf();
 	}
 
-	let config = {};
-	try {
-		config = await import(`file://${getFullPath('app.config.js')}`).then(
-			(data) => data.default
-		);
-	} catch (err) {
-		log.warn('No config found, use internals', 'Pineglade app');
-	}
-
-	const { buildScripts, buildStyles, lintScripts, lintSpaces } =
-		defineTasks(config);
-
-	if (!isDev) {
-		await Promise.all([lintSpaces(), lintScripts()]);
+	// Тестируем приложение перед сборкой
+	if (isCompile || isTest) {
+		await Promise.all([app.lintSpaces(), app.lintScripts()]);
 	}
 
 	if (isCompile) {
-		await Promise.all([buildScripts(), buildStyles()]);
+		await Promise.all([app.buildScripts(), app.buildStyles()]);
 	}
 })();
